0a9a487be5a6ce6a4a9beb3c6a263641879de20d
yuvanchanthar <151838268+yuvanchanthar@users.noreply.github.com>
2023-12-07 15:15:55 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES-DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 
 # Default primary key field type
